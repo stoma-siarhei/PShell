41b6b844a97adee24e616e1482452d0074fa79c8
Sergey Stoma <sergey.stoma@vba.domain.com>
2019-12-17 11:49:18 +0300

    Заголовки

--- SimpleHashFile/HashFile/pch.h
+++ SimpleHashFile/HashFile/pch.h
@@ -2,6 +2,18 @@
 #define _PCH_H_
 
 
+#include <tuple>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <filesystem>
+
 #include "framework.h"
 
 
